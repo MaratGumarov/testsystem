ed521045e478c20c279ba6aaa8cdadc3f897fd77
k.iskhakova <k.iskhakova@iis-soft.ru>
2016-11-08 10:53:47 +0300

    mvc-dispatcher-servlet + start page

--- src/main/java/com/gangofthree/testsystem/controller/Controller.java
+++ src/main/java/com/gangofthree/testsystem/controller/Controller.java
@@ -1,4 +1,13 @@
 package com.gangofthree.testsystem.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@org.springframework.stereotype.Controller
 public class Controller {
+
+  @RequestMapping(value = "/")
+   public ModelAndView showStart() {
+    return new ModelAndView("Login");
+  }
 }
